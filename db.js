ce87aded0db52201373f97a40c68b26a0f562580
Irmvvn_D_Vinci <irmuun6789@gmail.com>
2026-06-08 21:24:01 +0200

    Refactor database connection to use environment variables

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
-const mysql=require ("mysql2");
+const mysql = require("mysql2");
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Irmegun9576$",
-    database:"casino"
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
-db.connect((err)=>{
+
+db.connect((err) => {
     if(err){
         console.log("Database connection failed");
         console.log(err);
         return;
     }
+
     console.log("MYSQL connected!");
 });
-module.exports=db;
+
+module.exports = db;
